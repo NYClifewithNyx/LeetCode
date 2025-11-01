987074547dbcabc9b35dcee1efad70a455429209
뉴욕에서, 닉스_NYC Life with Nyx <jaewha0815@gmail.com>
2025-10-31 23:43:40 -0400

    Implement twoSum method using HashMap

---
+++ 1. TwoSum.java
@@ -0,0 +1,18 @@
+//got helped with ChatGPT b/c I am new to Map & HashMap
+//Runtime = 2ms, beats 99.08%
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[] {map.get(need), i};
+             }
+            map.put(nums[i], i);
+    }
+     return new int[] {};
+    }
+}
